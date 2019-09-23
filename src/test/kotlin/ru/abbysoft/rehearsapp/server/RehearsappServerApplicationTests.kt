c04033d297920254114f99c4a363272aec6bffce
ArtemPopof <Deviling42@gmail.com>
2019-09-23 15:07:33 +0300

    initial commit

---
+++ src/test/kotlin/ru/abbysoft/rehearsapp/server/RehearsappServerApplicationTests.kt
@@ -0,0 +1,16 @@
+package ru.abbysoft.rehearsapp.server
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RehearsappServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
