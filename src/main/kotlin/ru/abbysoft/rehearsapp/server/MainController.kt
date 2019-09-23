e73258172d3a146b79893f188e00bb11028487c3
ArtemPopof <Deviling42@gmail.com>
2019-09-23 16:39:50 +0300

    added one more path to rest service

--- src/main/kotlin/ru/abbysoft/rehearsapp/server/MainController.kt
+++ src/main/kotlin/ru/abbysoft/rehearsapp/server/MainController.kt
@@ -1,13 +1,31 @@
 package ru.abbysoft.rehearsapp.server
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class MainController {
 
+    private val random = Random(System.currentTimeMillis())
+
     @RequestMapping("/")
     fun index() : String {
-        return "This is Rehearsapp REST api index page! \n\n\n <a href='${APP_PAGE}'>about Rehearsapp</a>"
+        val randId = random.nextLong()
+        return """
+            This is Rehearsapp REST api index page!
+             <br>
+             <a href='${APP_PAGE}'>about Rehearsapp</a>
+             <br>
+             <a href='${PLACE_GET_METHOD_PATH}/?id=${randId}'>get Place</a>
+        """
+    }
+
+    @GetMapping(PLACE_GET_METHOD_PATH)
+    fun getPlace(@RequestParam("id") id : Long) : String {
+        // get place from db and return
+        return "place with id [${id}] is Saint-Petersburg Baltiyskaya house 88"
     }
 }
--- src/main/kotlin/ru/abbysoft/rehearsapp/server/Strings.kt
+++ src/main/kotlin/ru/abbysoft/rehearsapp/server/Strings.kt
@@ -1,3 +1,4 @@
 package ru.abbysoft.rehearsapp.server
 
 const val APP_PAGE = "https://github.com/ArtemPopof/RehearsalApp"
+const val PLACE_GET_METHOD_PATH = "/place"
