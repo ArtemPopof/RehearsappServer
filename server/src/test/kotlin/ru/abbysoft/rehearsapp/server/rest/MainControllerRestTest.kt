cf85ec09d41e3d739ed7a03a02b341c5977393a9
ArtemPopof <Deviling42@gmail.com>
2019-10-06 17:54:08 +0300

    added one more test for mainController

---
+++ server/src/test/kotlin/ru/abbysoft/rehearsapp/server/rest/MainControllerRestTest.kt
@@ -0,0 +1,50 @@
+package ru.abbysoft.rehearsapp.server.rest
+
+import org.hamcrest.collection.IsCollectionWithSize.hasSize
+import org.hamcrest.core.Is
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.BDDMockito.given
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import ru.abbysoft.rehearsapp.model.Place
+import ru.abbysoft.rehearsapp.server.MainController
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+
+private const val ID = 0L
+
+@RunWith(SpringRunner::class)
+@WebMvcTest(MainController::class)
+class MainControllerRestTest {
+
+    @MockBean
+    private lateinit var controller: MainController
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun testUnicodeNameForPlaceShouldBeReturnedCorrectly() {
+        val place = getPlaceWithRussianName()
+
+        given(controller.getPlace(ID)).willReturn(place)
+
+        mockMvc.perform(get("/place/0/").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("$.name", Is.`is`(place.name)))
+    }
+
+    private fun getPlaceWithRussianName(): Place {
+        val place = Place()
+        place.name = "Имя на русском"
+        return place
+    }
+}
