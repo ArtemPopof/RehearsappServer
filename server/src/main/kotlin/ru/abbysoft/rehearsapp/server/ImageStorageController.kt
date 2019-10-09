53727eeb30a31c43899ce5c17008c9682a36927c
ArtemPopof <Deviling42@gmail.com>
2019-10-09 15:05:15 +0300

    added import of new model

--- server/src/main/kotlin/ru/abbysoft/rehearsapp/server/ImageStorageController.kt
+++ server/src/main/kotlin/ru/abbysoft/rehearsapp/server/ImageStorageController.kt
@@ -3,6 +3,7 @@ package ru.abbysoft.rehearsapp.server
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
+import ru.abbysoft.rehearsapp.model.ImageControllerResponse
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileNotFoundException
